8b83ddc7d2fca4837e64ad356cb2c8d19f74ec8e
Uri Yakhini <uri.yakhini1@gmail.com>
2019-06-15 18:15:03 +0300

    implemented SearchBar

--- weather-app/src/components/App/App.js
+++ weather-app/src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h2>Weather App</h2>
+        <SearchBar/>
       </header>
     </div>
   );
---
+++ weather-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <input placeholder='location'></input>
+        );
+    } 
+}
+
+export default SearchBar;
