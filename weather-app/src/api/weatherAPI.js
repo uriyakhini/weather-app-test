351c9601b3ea480a3bb31ec982ae72f36f7b8e15
Uri Yakhini <uri.yakhini1@gmail.com>
2019-06-16 11:28:22 +0300

    Added basic api specs.

---
+++ weather-app/src/api/weatherAPI.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+// Openweathermap api key.
+const API_KEY="614e59b8b1d6d7d42b026b278ac63500";
+
+function getWeather(location) {
+    return;
+}
+
+export default getWeather;
